e03506083bca2da3363bd4edf2d493a8f8275645
Erik Smistad <ersmistad@gmail.com>
2020-01-14 11:07:28 +0100

    improved usage of script

--- convert.py
+++ convert.py
@@ -2,54 +2,65 @@ import pyproj
 import json
 import numpy
 from math import ceil
+import sys
 
-input_file = 'kommuner-utm33.geojson'
-output_file = 'kommuner.json'
-name = 'administrative_enheter.fylke'
-name = 'administrative_enheter.kommune'
+if __name__ == "__main__":
 
-coordinates_per_object = 2000
-keep_all = True
+    arguments = sys.argv
+    if len(arguments) < 3:
+        print('Usage: convert.py input-filename.json output-filename.json')
+        exit()
 
-#defineprojections
-source = pyproj.Proj(proj='utm', zone=33, ellps='WGS84')
-target = pyproj.Proj(init='epsg:3857')
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
 
-#load in data
-with open(input_file, 'r') as f:
-    data = json.load(f)
+    coordinates_per_object = 2000
+    keep_all = True
 
-data[name]['crs']['properties']['name'] = 'EPSG:3857'
-#traverse data in json string
-for feature in data[name]['features']:
-     #print feature['geometry']['type']
-     #print feature['geometry']['coordinates']
+    #defineprojections
+    source = pyproj.Proj(proj='utm', zone=33, ellps='WGS84')
+    target = pyproj.Proj(init='epsg:3857')
 
-    #all coordinates
-    coords = feature['geometry']['coordinates']
+    #load in data
+    with open(input_file, 'r') as f:
+        data = json.load(f)
 
-    #coordList is for each individual polygon
-    newCoordLists = []
-    for coordList in coords:
-        print(len(coordList))
-        step = int(ceil(len(coordList)/coordinates_per_object))
-        newCoordList = []
-        #each point
-        counter = 0
-        for coordPair in coordList:
-            if keep_all or counter % step == 0 or counter + 1 == len(coordList):
-                x1 = coordPair[0]
-                y1 = coordPair[1]
-                #do transformation
-                x, y = pyproj.transform(source,target,x1, y1)
-                #print(coordPair[0], coordPair[1])
-                x, y = source(x1, y1, inverse=True)
-                #print(x, y)
-                newCoordList.append([x, y])
-            counter += 1
+    if 'administrative_enheter.fylke' in data:
+        name = 'administrative_enheter.fylke'
+    else:
+        name = 'administrative_enheter.kommune'
 
-        newCoordLists.append(newCoordList)
-    feature['geometry']['coordinates'] = newCoordLists
+    data[name]['crs']['properties']['name'] = 'EPSG:3857'
+    #traverse data in json string
+    for feature in data[name]['features']:
+         #print feature['geometry']['type']
+         #print feature['geometry']['coordinates']
 
-with open(output_file, 'w') as f:
-    f.write(json.dumps(data[name]))
+        #all coordinates
+        coords = feature['geometry']['coordinates']
+
+        #coordList is for each individual polygon
+        newCoordLists = []
+        for coordList in coords:
+            print(len(coordList))
+            step = int(ceil(len(coordList)/coordinates_per_object))
+            newCoordList = []
+            #each point
+            counter = 0
+            for coordPair in coordList:
+                if keep_all or counter % step == 0 or counter + 1 == len(coordList):
+                    x1 = coordPair[0]
+                    y1 = coordPair[1]
+                    #do transformation
+                    x, y = pyproj.transform(source,target,x1, y1)
+                    #print(coordPair[0], coordPair[1])
+                    x, y = source(x1, y1, inverse=True)
+                    #print(x, y)
+                    newCoordList.append([x, y])
+                counter += 1
+
+            newCoordLists.append(newCoordList)
+        feature['geometry']['coordinates'] = newCoordLists
+
+    with open(output_file, 'w') as f:
+        f.write(json.dumps(data[name]))
